7b82e46538d00fbbcb3cd0301bf02368bfb771c5
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-01-15 13:09:57 +0800

    first commit

---
+++ models/blog.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+// insert toJSON code here
+
+module.exports = mongoose.model("Blog", blogSchema);
