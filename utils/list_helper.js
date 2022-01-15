b9184e76a9f4cd1c19cb7f5449b79ee13d890d27
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-01-15 15:33:03 +0800

    completed exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  var numOfLikes = 0;
+
+  for (let i = 0; i < blogs.length; i++) {
+    numOfLikes += blogs[i].likes;
+  }
+
+  return numOfLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
